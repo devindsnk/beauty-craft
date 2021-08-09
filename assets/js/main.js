f3eeb444302c846536f6cdb487cc5550ce981781
daredevil25 <devindsnk25@gmail.com>
2021-08-10 00:40:48 +0530

    Adding receptionist dashboard and init. css

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// (function ($) {
+//    $('.sidebar-menu_item-link').on('click', function (e) {
+//       // e.preventDefault();
+//       console.log(e.parent())
+//       var itemTarget = $(this).data('target');
+
+      
+
+//       // if (closeAllExceptThis) {
+//       //    closedItems = [];
+//       //    $('.u-sidebar-nav-menu--top-level ul:not("' + itemTarget + '")').slideUp(400).parent().removeClass('u-sidebar-nav--opened');
+//       //    $(itemTarget).slideToggle(400).parent().toggleClass('u-sidebar-nav--opened');
+//       //    if ($(this).parent().hasClass('u-sidebar-nav--opened')) {
+//       //       closedItems.push(itemTarget);
+//       //    }
+
+//       //    if ($body.hasClass('tablet-mode')) {
+//       //       $body.toggleClass('has-tablet-opened-items');
+//       //    }
+//       // } else {
+//       //    closedItems.push(itemTarget);
+//       //    $(itemTarget).slideToggle(400).parent().toggleClass('u-sidebar-nav--opened');
+
+//       //    if ($body.hasClass('tablet-mode')) {
+//       //       $body.toggleClass('has-tablet-opened-items');
+//       //    }
+//       // }
+//    });
+// })(jQuery);
+
+$(document).ready(function () {
+   alert("Clicked");
+});
