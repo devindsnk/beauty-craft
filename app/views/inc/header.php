07239c19aa5c5cf8fdcae6b1d39138dc59b45cd9
Ruwanthi123 <ruwanthimunasinghe42@gmail.com>
2021-10-05 09:56:49 +0530

    create customer account form added

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -15,6 +15,8 @@
    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/sideNav.css" />
    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/layoutTemplate1.css" />
    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/modalStyle.css"/>
+   <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/sysAdmin.css"/>
+  
    
 
    <!-- Temporary Styles-->
