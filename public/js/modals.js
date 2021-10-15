e26a90d4e394e5a199107c8ee4ecfe41536e5679
Ruwanthi123 <ruwanthimunasinghe42@gmail.com>
2021-10-15 19:07:14 +0530

    devin danna epa kiwwa

--- public/js/modals.js
+++ public/js/modals.js
@@ -33,7 +33,7 @@ removeCustomerBtnList.forEach((btn) => {
     });
 });
 
-// Edit Leave Modal Section
+// Edit request Leave Modal Section
 const editLeaveModal = document.querySelector(".edit-leave");
 const editLeaveBtnList = document.querySelectorAll(".btnEditLeave");
 console.log(editLeaveModal);
@@ -46,7 +46,7 @@ if (editLeaveBtnList) {
     });
 }
 
-// Delete Leave Modal Section
+// Delete request Leave Modal Section
 const deleteLeaveModal = document.querySelector(".delete-leave");
 const deleteLeaveBtnList = document.querySelectorAll(".btnDeleteLeave");
 console.log(deleteLeaveModal);
@@ -63,84 +63,84 @@ if (deleteLeaveBtnList) {
 const resRecallModal = document.querySelector('.reservation-recall');
 const resRecallBtn = document.querySelector('.btnResRecall');
 resRecallBtn.addEventListener("click",
-   function () {
-      toggleModal();
-      modalToToggle = resRecallModal;
-      toggleModal();
-   }
+    function() {
+        toggleModal();
+        modalToToggle = resRecallModal;
+        toggleModal();
+    }
 );
 
 // Take Leave Modal Section
 const takeLeaveModal = document.querySelector('.take-leave');
 const takeLeaveBtnList = document.querySelector('.btnTakenLeave');
 takeLeaveBtnList.addEventListener("click",
-   function () {
-      modalToToggle = takeLeaveModal;
-      toggleModal();
-   }
+    function() {
+        modalToToggle = takeLeaveModal;
+        toggleModal();
+    }
 );
 
 // Edit taken Leave Modal Section
 const editTakenLeaveModal = document.querySelector('.edit-taken-leave');
 const editTakeLeaveBtnList = document.querySelectorAll('.btnEditTakenLeave');
 editTakeLeaveBtnList.forEach(btn => {
-   btn.addEventListener("click",
-      function () {
-         modalToToggle = editTakenLeaveModal;
-         toggleModal();
-      }
-   );
+    btn.addEventListener("click",
+        function() {
+            modalToToggle = editTakenLeaveModal;
+            toggleModal();
+        }
+    );
 });
 
 // Edit taken Leave Modal Section
 const deleteTakenLeaveModal = document.querySelector('.delete-taken-leave');
 const deleteTakeLeaveBtnList = document.querySelectorAll('.btnDeleteTakenLeave');
 deleteTakeLeaveBtnList.forEach(btn => {
-   btn.addEventListener("click",
-      function () {
-         modalToToggle = deleteTakenLeaveModal;
-         toggleModal();
-      }
-   );
-const resRecallModal = document.querySelector(".reservation-recall");
-const resRecallBtn = document.querySelector(".btnResRecall");
-// console.log(resRecallModal);
-if (resRecallBtn) {
-    resRecallBtn.addEventListener("click", function() {
-        toggleModal();
-        modalToToggle = resRecallModal;
-        toggleModal();
-    });
-}
+            btn.addEventListener("click",
+                function() {
+                    modalToToggle = deleteTakenLeaveModal;
+                    toggleModal();
+                }
+            );
+            const resRecallModal = document.querySelector(".reservation-recall");
+            const resRecallBtn = document.querySelector(".btnResRecall");
+            // console.log(resRecallModal);
+            if (resRecallBtn) {
+                resRecallBtn.addEventListener("click", function() {
+                    toggleModal();
+                    modalToToggle = resRecallModal;
+                    toggleModal();
+                });
+            }
 
-//Leave Request Modal Secrtion
-const leaveRequestModal = document.querySelector(".leaverequest");
-const leaveRequestBtn = document.querySelector(".btnleaveRequest");
-// console.log(leaveRequestModal);
-if (leaveRequestBtn) {
-    leaveRequestBtn.addEventListener("click", function() {
-        modalToToggle = leaveRequestModal;
-        toggleModal();
-    });
-}
+            //Leave Request Modal Secrtion
+            const leaveRequestModal = document.querySelector(".leaverequest");
+            const leaveRequestBtn = document.querySelector(".btnleaveRequest");
+            // console.log(leaveRequestModal);
+            if (leaveRequestBtn) {
+                leaveRequestBtn.addEventListener("click", function() {
+                    modalToToggle = leaveRequestModal;
+                    toggleModal();
+                });
+            }
 
-// Reservation More info Modal Section
-const resMoreInfoModal = document.querySelector(".reservation-more-info");
-const resMoreInfoBtnList = document.querySelectorAll(".btnResMoreInfo");
-console.log(resMoreInfoBtnList);
-resMoreInfoBtnList.forEach((btn) => {
-    btn.addEventListener("click", function() {
-        modalToToggle = resMoreInfoModal;
-        toggleModal();
-    });
-});
+            // Reservation More info Modal Section
+            const resMoreInfoModal = document.querySelector(".reservation-more-info");
+            const resMoreInfoBtnList = document.querySelectorAll(".btnResMoreInfo");
+            console.log(resMoreInfoBtnList);
+            resMoreInfoBtnList.forEach((btn) => {
+                btn.addEventListener("click", function() {
+                    modalToToggle = resMoreInfoModal;
+                    toggleModal();
+                });
+            });
 
-// Common section for all close buttons
-const btnCloseList = document.querySelectorAll(".btnClose");
-btnCloseList.forEach((btn) => {
-    btn.addEventListener("click", toggleModal);
-});
-// Common section for all modal toggle operations
-function toggleModal() {
-    modalToToggle.classList.toggle("show");
-}
+            // Common section for all close buttons
+            const btnCloseList = document.querySelectorAll(".btnClose");
+            btnCloseList.forEach((btn) => {
+                btn.addEventListener("click", toggleModal);
+            });
+            // Common section for all modal toggle operations
+            function toggleModal() {
+                modalToToggle.classList.toggle("show");
+            }
