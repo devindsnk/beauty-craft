5dcba7cc368f174d61597ec6fe19441632df04cc
daredevil25 <devindsnk25@gmail.com>
2021-11-06 09:57:02 +0530

    Added data binding functions to the core model

--- app/models/testModel.php
+++ app/models/testModel.php
@@ -1,61 +1,57 @@
 <?php
 class testModel extends Model
 {
-   // public function getAllUsers()
-   // {
-   //    $x = "Hair Trrrrr";
-   //    $y = 5;
-
-   //    $result  = null;
-
-   //    try
-   //    {
-   //       $this->dbh->beginTransaction();
-
-   //       $this->insert('resources', ['name' => "Test", 'quantity' => 4]);
-   //       $this->insert('resourcess', ['name' => $x, 'quantity' => $y]);
-
-   //       $this->dbh->commit();
-   //    }
-   //    catch (\Throwable $e)
-   //    {
-   //       print_r($e->getMessage());
-   //       $this->dbh->rollBack();
-   //       // throw $e; 
-   //    }
-
-
-   //    // $result = $this->getResultSet('staff', ['fName', 'lName', 'gender', 'staffType'], ['gender' => 'F']);
-   //    // $result = $this->customQuery("SELECT * FROM services");
-   //    // $result = $this->update('customers', ['fName' => 'Binuka', 'lName' => 'Karunarathne'], ['mobileNo' => '0712334567', 'gender' => 'M']);
-   //    // $result = $this->getRowCount('users', [null]);
-   //    // var_dump($result);
-   //    // return $result;
-   // }
-
-   public function func()
+   public function selectStmnt()
    {
-      $this->insert('resources', ['name' => "Test1", 'quantity' => 4]);
+      // To get a single record tableName, requiredColumns and conditions are passed
+      $results = $this->getSingle('users', '*', ['userType' => 6]);
+      var_dump($results);
+
+      // To get the all records tableName, requiredColumns and conditions are passed
+      $results = $this->getResultSet('staff', ['fName', 'lName', 'gender', 'staffType'], ['gender' => 'M']);
+      var_dump($results);
+
+      // To get the row count tableName and conditions are passed
+      $results = $this->getRowCount('users', ['userType' => 6, 'mobileNo' => '0717679714']);
+      var_dump($results);
    }
 
-   // public function getUsersCount()
-   // {
-   //    $result = $this->getRowCount('users', ['userType' => 6, 'mobileNo' => '0717679714']);
-   //    var_dump($result);
-   // }
+   public function insertStmnt()
+   {
+      // To insert a record tableName and valuesToBeInserted are passed
+      $results =  $this->insert('resources', ['name' => "Hair Trimmer", 'quantity' => 5]);
+      var_dump($results);
+   }
 
-   // public function getSingleUser()
-   // {
-   //    $result = $this->getSingle('users', '*', ['userType' => 6]);
-   //    var_dump($result);
-   //    return $result;
-   // }
+   public function updateStmnt()
+   {
+      // To update records tableName, valuesToBeUpdated and then conditions are passed
+      $results = $this->update('customers', ['fName' => 'Binuka', 'lName' => 'Karunarathne'], ['mobileNo' => '0712334567']);
+      var_dump($results);
+   }
 
-   // public function removeResource()
-   // {
-   //    $this->insert('resources', ['name' => "Hair Trimmer", 'quantity' => 5]);
-   //    // $this->delete('resources', ['name' => 'Hair Irons']);
-   //    // var_dump($result);
-   //    // return $result;
-   // }
+   public function deleteStmnt()
+   {
+      // To delete records tableName and conditions are passed
+      $results = $this->delete('resources', ['name' => 'Hair Irons']);
+      var_dump($results);
+   }
+
+   public function customStmnt()
+   {
+      // When using custom queries 
+      //    put the SQL statement as first paramater
+      //    put an array containg values to bind as the second parameter
+
+      $x = 1;
+      $y = 40;
+      $result = $this->customQuery(
+         "SELECT * FROM services WHERE status = :status AND totalDuration > :duration",
+         [
+            ':status' => $x,
+            ':duration' => $y
+         ]
+      );
+      var_dump($result);
+   }
 }
