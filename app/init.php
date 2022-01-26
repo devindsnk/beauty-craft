bdcce488abe3fd20afb6ca8643020802c2ccdf9c
daredevil25 <devindsnk25@gmail.com>
2022-01-26 10:28:05 +0530

    Init file name changes

--- app/init.php
+++ app/init.php
@@ -4,13 +4,13 @@
 require_once 'config/config.php';
 
 // Loading helpers
-require_once 'helpers/url_helper.php';
+require_once 'helpers/URL_helper.php';
 require_once 'helpers/Session_helper.php';
-require_once 'helpers/data_validation_helper.php';
-require_once 'helpers/sms_helper.php';
-require_once 'helpers/dateTimeExtended_helper.php';
-require_once 'helpers/toast_helper.php';
-require_once 'helpers/systemLog_helper.php';
+require_once 'helpers/DataValidation_helper.php';
+require_once 'helpers/SMS_helper.php';
+require_once 'helpers/DateTimeExtended_helper.php';
+require_once 'helpers/Toast_helper.php';
+require_once 'helpers/SystemLog_helper.php';
 
 spl_autoload_register(function ($className)
 {
